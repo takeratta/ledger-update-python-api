8927ca3104bb2a1aee7d36ed2b5f33b7210496be
PIERRE POLLASTRI <pollastri.p@gmail.com>
2016-10-12 11:04:23 +0200

    Add notes management

--- ledgerUpdateApi.py
+++ ledgerUpdateApi.py
@@ -1,8 +1,38 @@
-
 import yaml
+import hashlib
+
+def loadNotes(lang, filename):
+	result = None
+	try:
+		f = open("assets/notes/" + lang + "/" + filename, "r")
+		result = f.read()
+		f.close()
+	except:
+		print "WARNING: Unable to load notes for " + lang + "/" + filename
+	if (result == None and lang != "en"):
+		return loadNotes("en", filename)
+	elif (result != None):
+		return result
+	else:
+		return None
+
+def inflateItems(items, lang):
+	for key, category in items.iteritems():
+		for item in category:
+			if (item.get("notes") != None):
+				item["notes"] = loadNotes(lang, item["notes"])
+				if (item["notes"] == None): item.pop("notes", None)
+			if (item.get("app") != None):
+				item["identifier"] = hashlib.sha256(str(item["app"])).hexdigest()
+	return items
+
+def inflateConfig(config, params):
+	config["firmwares"] = inflateItems(config["firmwares"], params.get("lang", "en")[0])
+	config["applications"] = inflateItems(config["applications"], params.get("lang", "en")[0])
+	return config
 
 lastConfig = None
-def getConfig(provider):
+def getConfig(provider, params):
 	config = None
 	filename = "config"
 	if (len(provider) != 0):
@@ -14,7 +44,7 @@ def getConfig(provider):
 		print "WARNING: An error happened during config loading."
 		return lastConfig;
 	elif (config != None):
-		lastConfig = config
+		lastConfig = inflateConfig(config, params)
 		return config
 	else:
 		raise Exception("Unable to load config file")
@@ -26,13 +56,13 @@ class LedgerUpdateAPI:
 
 	def getFirmwares(self, handler, path, params):
 		try:
-			return (200, getConfig(params.get("provider", [""])[0])["firmwares"])
+			return (200, getConfig(params.get("provider", [""])[0], params)["firmwares"])
 		except:
 			return (404, {"error": "Provider not found"})
 
 	def getApplications(self, handler, path, params):
 		try:
-			return (200, getConfig(params.get("provider", [""])[0])["applications"])
+			return (200, getConfig(params.get("provider", [""])[0], params)["applications"])
 		except:
 			return (404, {"error": "Provider not found"})
 
--- updateServer.py
+++ updateServer.py
@@ -15,19 +15,51 @@ class LedgerUpdateHTTPRequestHandler(BaseHTTPRequestHandler):
 		"/update/applications": api.getApplications,
 	}
 
+	assets = {
+		"/update/assets/icons/": ('.png', 'image/png', './assets/icons/')
+	}
+
+	def handle_assets(self, path):
+		url = urlparse(self.path)
+		for key, value in self.assets.iteritems():
+			print(key)
+			print(path)
+			print(path.startswith(key))
+			print(value[2] + path.split('/')[-1] + value[0])
+			if (path.startswith(key)):
+				try:
+					print(value[2] + path.split('/')[-1] + value[0])
+					file = open(value[2] + path.split('/')[-1] + value[0], 'rb')
+				 	self.send_response(200)
+				 	self.send_header('Content-type', value[1])
+				 	self.end_headers()
+				 	self.wfile.write(file.read())
+				 	file.close()
+				except Exception as exception:
+					answer = api.notFound(self, url.path, parse_qs(url.query))
+					self.send_response(answer[0])
+					self.send_header('Content-type','application/json')
+					self.end_headers()
+					self.wfile.write(json.dumps(answer[1]))
+				return True
+		return False
+
 	def do_GET(self):
 		url = urlparse(self.path)
-		#send code 200 response
-		answer = self.endpoints.get(url.path, api.notFound)(self, url.path, parse_qs(url.query))
-			
-		self.send_response(answer[0])
 
-		#send header first
-		self.send_header('Content-type','application/json')
-		self.end_headers()
+		# Check if we request an asset first
+		if (self.handle_assets(url.path) == False):
+			#send code 200 response
+			answer = self.endpoints.get(url.path, api.notFound)(self, url.path, parse_qs(url.query))
+				
+			self.send_response(answer[0])
 
-		#send file content to client
-		self.wfile.write(json.dumps(answer[1]))
+			#send header first
+			self.send_header('Content-type','application/json')
+			self.end_headers()
+
+			#send file content to client
+			self.wfile.write(json.dumps(answer[1]))
 		return
 
 def run():
